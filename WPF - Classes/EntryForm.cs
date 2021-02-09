0a7bd0ff1232737d640166c7f6be0caa364a9b59
jJessica Wren <jessica.wren@ou.edu>
2021-02-09 15:07:53 -0600

    Started P WPF classes

---
+++ WPF - Classes/EntryForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF___Classes
+{
+    class EntryForm
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+
+
+        /// <summary>
+        /// Default ctor
+        /// </summary>
+        public EntryForm()
+        {
+            Name = string.Empty;
+            Address = string.Empty;
+            ZipCode = 0;
+        }
+        
+        /// <summary>
+        /// Overloaded ctor
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="address"></param>
+        /// <param name="zipCode"></param>
+        public EntryForm(string name, string address, int zipCode)
+        {
+            Name = name;
+            Address = address;
+            ZipCode = zipCode;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
