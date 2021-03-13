d68499a2c8855183db83cf6b7eed158414a8e444
jJessica Wren <jessica.wren@ou.edu>
2021-03-13 15:01:04 -0600

    Finished and started GOT Quote (p)

---
+++ GOT Quote/QuoteAPI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GOT_Quote
+{
+    class QuoteAPI // first brace
+    {
+        public string quote { get; set; }
+        public string character { get; set; }
+
+    }
+}
